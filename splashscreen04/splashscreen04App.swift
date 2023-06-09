d86f28b1ac416bf251a776275f797b9b8ad9fe92
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-10 00:46:32 +0530

    Initial Commit

---
+++ splashscreen04/splashscreen04App.swift
@@ -0,0 +1,17 @@
+//
+//  splashscreen04App.swift
+//  splashscreen04
+//
+//  Created by Lucas Pereira on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct splashscreen04App: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
